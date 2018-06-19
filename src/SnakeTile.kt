aee3050bef076c10aae09db7680c03ba2d2ccd0f
Lukas Hejnal <hejnaluk@fel.cvut.cz>
2018-06-19 23:26:53 +0200

    Create base class for tile and basic functionality, basic tests

---
+++ src/SnakeTile.kt
@@ -0,0 +1,29 @@
+class SnakeTile private constructor (initX : Int, initY : Int, initDirection : Direction){
+
+    var x : Int = initX
+    var y : Int = initY
+    var direction : Direction = initDirection
+
+
+    companion object {
+        fun createTile ( x : Int, y : Int, direction : Direction ) : SnakeTile {
+            return SnakeTile( x, y, direction)
+        }
+    }
+
+    /**
+     * Move tile
+     */
+    fun move( newDirection : Direction ) {
+        when (direction) {
+            Direction.LEFT -> x--
+            Direction.RIGHT -> x++
+            Direction.UP -> y--
+            Direction.DOWN -> y++
+        }
+
+        if ( !Direction.isOpposite(direction, newDirection)) {
+            direction = newDirection
+        }
+    }
+}
