939d2d02664d3e6dc22344ee0293ebc196e9886b
Lukas Hejnal <hejnaluk@fel.cvut.cz>
2018-06-23 14:56:17 +0200

    First functional prototype, use LibKTX

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,16 @@
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Input
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import com.badlogic.gdx.graphics.*
+import com.badlogic.gdx.graphics.g2d.BitmapFont
+import com.badlogic.gdx.graphics.g2d.Sprite
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import ktx.app.KtxApplicationAdapter
+import ktx.app.KtxInputAdapter
+import ktx.graphics.rect
+import ktx.graphics.use
 
 object MainKt {
     @JvmStatic fun main(args: Array<String>) {
@@ -20,12 +30,68 @@ object MainKt {
         }
 }
 
-class Runner : KtxApplicationAdapter {
+class Runner : KtxApplicationAdapter, KtxInputAdapter {
+
+    lateinit var font : BitmapFont
+    lateinit var batch : SpriteBatch
+    lateinit var camera : Camera
+    lateinit var renderer : ShapeRenderer
+
+    var xPos : Float = 0F
+    var delta : Float = 0F
+
     override fun create() {
         super.create()
+
+        font = BitmapFont()
+        batch = SpriteBatch()
+        renderer = ShapeRenderer()
+
+        font.color = Color.WHITE
+        camera = OrthographicCamera(800F,600F)
+        camera.position.set(400F, 400F, 0F)
+        camera.update()
+
+        Gdx.input.inputProcessor = this
+    }
+
+
+    override fun dispose() {
+        super.dispose()
     }
 
     override fun render() {
+        Gdx.gl.glClearColor(0F, 0F, 0F, 0F)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        renderer.run {
+            projectionMatrix = camera.combined
+            begin(ShapeRenderer.ShapeType.Filled)
+            color = Color.WHITE
+            rect(calculateXPos(), 200F, 10F, 10F)
+            end()
+        }
         super.render()
+    }
+
+    private fun calculateXPos(): Float {
+        xPos += delta
+        return xPos
+    }
+
+    override fun keyDown(keycode: Int): Boolean {
+        delta = if ( keycode == Input.Keys.A || keycode == Input.Keys.LEFT) {
+            -1F
+        } else if ( keycode == Input.Keys.D || keycode == Input.Keys.RIGHT) {
+            1F
+        } else {
+            delta
+        }
+
+        return true
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        return true
     }
 }
