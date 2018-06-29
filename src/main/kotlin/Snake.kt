3322c3f42c7cb8048cd5306650d918b11fb79d4e
hejnall1 <kejhy93@gmail.com>
2018-06-29 18:53:56 +0200

    Remove multiple rotation per move

--- src/main/kotlin/Snake.kt
+++ src/main/kotlin/Snake.kt
@@ -1,59 +1,69 @@
 import java.util.*
 
-class Snake private constructor(val initX : Int, val initY : Int) {
+class Snake private constructor(val initX: Int, val initY: Int) {
 
-    var x : Int = initX
-        get() = listOfTiles.firstOrNull()?.x?:initX
+    var x: Int = initX
+        get() = synchronized(listOfTiles) { listOfTiles.firstOrNull()?.x ?: initX }
 
-    var y : Int = initY
-        get() = listOfTiles.firstOrNull()?.y?:initY
+    var y: Int = initY
+        get() = synchronized(listOfTiles) { listOfTiles.firstOrNull()?.y ?: initY }
 
-    var direction : Direction = Direction.LEFT
-        get() = listOfTiles.firstOrNull()?.direction?:Direction.LEFT
+    var direction: Direction = Direction.LEFT
+        get() = synchronized(listOfTiles) { listOfTiles.firstOrNull()?.direction ?: Direction.LEFT }
 
-    var listOfTiles : MutableList<SnakeTile> = LinkedList()
+    var listOfTiles: MutableList<SnakeTile> = LinkedList()
+
+    var canRotate : Boolean = true
 
     init {
         listOfTiles.add(SnakeTile.createTile(x, y, Direction.RIGHT))
+        addTile()
+        addTile()
+        addTile()
+        addTile()
     }
 
     companion object {
-        fun createSnake(x : Int, y : Int ) : Snake {
+        fun createSnake(x: Int, y: Int): Snake {
             return Snake(x, y)
         }
     }
 
     fun move() {
-        var currDirection = listOfTiles.first().direction
-        for ( tile in listOfTiles ) {
-            var nextDirection = tile.direction
-            tile.move(currDirection)
-            currDirection = nextDirection
+        synchronized(listOfTiles) {
+            var currDirection = listOfTiles.first().direction
+            for (tile in listOfTiles) {
+                val nextDirection = tile.direction
+                tile.move(currDirection)
+                currDirection = nextDirection
+            }
         }
+
+        canRotate = true
     }
 
     fun addTile() {
-        var lastTile = listOfTiles.last()
-        var direction = lastTile.direction
+        val lastTile = listOfTiles.last()
+        val direction = lastTile.direction
 
-        var positionNextTileX = calculateNextTileX(lastTile.x, direction)
-        var positionNextTileY = calculateNextTileY(lastTile.y, direction)
+        val positionNextTileX = calculateNextTileX(lastTile.x, direction)
+        val positionNextTileY = calculateNextTileY(lastTile.y, direction)
 
         listOfTiles.add(SnakeTile.createTile(positionNextTileX, positionNextTileY, direction))
     }
 
     private fun calculateNextTileY(y: Int, direction: Direction): Int {
-        return when ( direction ) {
-            Direction.UP -> y-1
-            Direction.DOWN -> y+1
+        return when (direction) {
+            Direction.UP -> y - 1
+            Direction.DOWN -> y + 1
             else -> y
         }
     }
 
-    private fun calculateNextTileX(x : Int, direction: Direction): Int {
-        return when ( direction ) {
-            Direction.LEFT -> x+1
-            Direction.RIGHT -> x-1
+    private fun calculateNextTileX(x: Int, direction: Direction): Int {
+        return when (direction) {
+            Direction.LEFT -> x + 1
+            Direction.RIGHT -> x - 1
             else -> x
         }
     }
@@ -61,28 +71,32 @@ class Snake private constructor(val initX : Int, val initY : Int) {
     fun turnLeft() {
         val currDirection = direction
 
-        if ( !Direction.isOpposite(currDirection, Direction.LEFT)) {
+        if (!Direction.isOpposite(currDirection, Direction.LEFT)) {
             listOfTiles.first().direction = Direction.LEFT
         }
     }
+
     fun turnRight() {
         val currDirection = direction
 
-        if  ( !Direction.isOpposite(currDirection, Direction.RIGHT)) {
+        if (!Direction.isOpposite(currDirection, Direction.RIGHT)) {
             listOfTiles.first().direction = Direction.RIGHT
         }
     }
+
     fun turnUp() {
         val currDirection = direction
 
-        if ( !Direction.isOpposite(currDirection, Direction.UP)) {
+        if (!Direction.isOpposite(currDirection, Direction.UP)) {
             listOfTiles.first().direction = Direction.UP
         }
+
     }
+
     fun turnDown() {
         val currDirection = direction
 
-        if ( !Direction.isOpposite(currDirection, Direction.DOWN)) {
+        if (!Direction.isOpposite(currDirection, Direction.DOWN)) {
             listOfTiles.first().direction = Direction.DOWN
         }
     }
